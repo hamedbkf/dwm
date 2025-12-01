2d3463cc75da7b0c3e7f7a8c2d64e67bd6748b62
Hamed <hamedbk@getgoogleoff.me>
2025-12-01 23:50:15 +0100

    Change default font size to 14, weight to regular

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:pixelsize=16:weight=bold" };
+static const char *fonts[]          = { "terminus:pixelsize=14:weight=regular" };
 static const char col_white[]       = "#ffffff";
 static const char col_black[]       = "#000000";
 static const char col_gray[]        = "#dedede";
